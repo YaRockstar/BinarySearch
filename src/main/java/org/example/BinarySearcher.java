8ccd72aae8c611ac6cc1811a0753bec189574e68
ya_rockstar <yekalashnikov@gmail.com>
2023-08-30 00:16:40 +0300

    realize iterative and recursive binary search methods

--- src/main/java/org/example/BinarySearcher.java
+++ src/main/java/org/example/BinarySearcher.java
@@ -1,2 +1,58 @@
-package org.example;public class BinarySearcher {
+package org.example;
+
+public class BinarySearcher {
+
+    /**
+     * Этот метод возвращает индекс найденного элемента, в противном случае возвращает -1.
+     * Реализация итеративная (через цикл).
+     * */
+    public static int iterativeBinarySearch(int[] array, int value) {
+        int low = 0;
+        int high = array.length;
+
+        do {
+            int middleIndex = (low + high) / 2;
+            var middle = array[middleIndex];
+
+            if (middle == value) {
+                return middleIndex;
+            }
+
+            if (middle < value) {
+                low = middleIndex + 1;
+            } else {
+                high = middleIndex;
+            }
+
+        } while (low < high);
+
+        return -1;
+    }
+
+    /**
+     * Этот метод возвращает индекс найденного элемента, в противном случае возвращает -1.
+     * Использует внутренний рекурсивный метод internalRecursiveBinarySearch.
+     * */
+    public static int recursiveBinarySearch(int[] array, int value) {
+        return internalRecursiveBinarySearch(array, value, 0, array.length);
+    }
+
+    private static int internalRecursiveBinarySearch(int[] array, int value, int low, int high) {
+        if (low >= high) {
+            return -1;
+        }
+
+        int middleIndex = (low + high) / 2;
+        var middle = array[middleIndex];
+
+        if (middle == value) {
+            return middleIndex;
+        }
+
+        if (middle < value) {
+            return internalRecursiveBinarySearch(array, value, middleIndex + 1, high);
+        } else {
+            return internalRecursiveBinarySearch(array, value, low, middleIndex);
+        }
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,12 @@
-package org.example;public class Main {
+package org.example;
+
+import static org.example.BinarySearcher.*;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var array = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        var target = 2;
+        System.out.println(iterativeBinarySearch(array, target));
+        System.out.println(recursiveBinarySearch(array, target));
     }
 }
